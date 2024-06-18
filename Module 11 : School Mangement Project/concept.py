72c530d1a873f2fa64d4bd2880f33424a7636b60
Md. Uzzal Mia <129536126+uzzal2200@users.noreply.github.com>
2024-06-18 11:55:23 +0600

    Add files via upload

---
+++ Module 11 : School Mangement Project/concept.py
@@ -0,0 +1,24 @@
+"""
+
+------------------------------------
+
+    HIGH LEVEL AREAS
+
+------------------------------------
+school system development
+1.student management
+2.Teacher Mangement
+3.Cageteria
+4.Sports Mangement
+5.Club Mangement
+6.Even Mangement
+7.Libray Management
+8.Assessment
+---------------------------------------
+Core Funtionalities
+1. There will be a classroom consisting of multiple students
+2.Student can enroll multpile classes
+3.Teachers will take classes, they will take exams, they grade the exams
+4.System will generate the overall grade
+
+"""
