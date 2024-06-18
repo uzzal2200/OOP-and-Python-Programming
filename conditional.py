f1ee1811a9aa60706579d0acc5914db1725ffe47
Md. Uzzal Mia <129536126+uzzal2200@users.noreply.github.com>
2024-06-18 11:31:26 +0600

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,26 @@
+# < , > <= , >= , == , !== , /= 
+a = 1
+
+# if(a > 2):
+#     print('2 er cheye pac boro')
+# elif(a==1):
+#     print('that is equal')
+# else:
+#     print(' 2 cheye chot')
+# boss = False
+# if boss is True:
+#     print('no')
+# else:
+#     print('yes')
+# Nested if
+boss = True
+coin='head'
+if boss is True:
+    print('boss you are joss')
+    if coin == 'tail':
+        print('Batting')
+
+    else:
+        print('Bolling')
+else:
+    print('boss are bad')
