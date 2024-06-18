202840b44a56b879fcf2f1755248969c5c43fbf1
Md. Uzzal Mia <129536126+uzzal2200@users.noreply.github.com>
2024-06-18 11:31:51 +0600

    Delete Module 01 : Introduction to Python directory

--- Module 01 : Introduction to Python/first.py
+++
@@ -1,4 +0,0 @@
-print('kopa python kopa')
-print(220605)
-print(45)
-print(497)
--- Module 01 : Introduction to Python/operators.py
+++
@@ -1,9 +0,0 @@
-num1 = 45
-num2 = 3
-print(num1+num2)
-print(num1-num2)
-print(num1*num2)
-print(num1 / num2)
-print(num1 % num2)   
-print(num1 // num2)   # porno bagshes
-print(10 // 3)
